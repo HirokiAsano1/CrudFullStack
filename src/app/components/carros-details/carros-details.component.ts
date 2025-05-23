9c57a9f455fa9069e7fcd7fc6c74abeffe84f3b6
hirokiasano1 <hirokiasanovieira@gmail.com>
2025-05-22 22:30:09 -0300

    feat : Layout

--- src/app/components/carros-details/carros-details.component.ts
+++ src/app/components/carros-details/carros-details.component.ts
@@ -1,23 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Carro } from '../../models/carro';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carros-details',
-  imports: [MdbFormsModule,FormsModule],
+  imports: [MdbFormsModule, FormsModule, CommonModule],
   templateUrl: './carros-details.component.html',
-  styleUrl: './carros-details.component.scss'
+  styleUrl: './carros-details.component.scss',
 })
-
-
 export class CarrosDetailsComponent {
+  carro: Carro = new Carro(0, '');
+  router = inject(ActivatedRoute);
+  router2 = inject(Router);
+  constructor() {
+    let id = this.router.snapshot.params['id'];
+    if (id > 0) {
+      this.findById(id);
+    }
+  }
 
-    carro: Carro = new Carro(0, "") 
-  save()
-  {
-    alert("ADD COM SUCESSO")
+  findById(id: number) {
+    let carroRetornado: Carro = new Carro(id, 'Fiesta');
+    this.carro = carroRetornado;
+  }
+  save() {
+    if (this.carro.id > 0) {
+      Swal.fire({
+        title: 'Sucesso!',
+        text: 'Editado COM SUCESSO',
+        icon: 'success',
+        confirmButtonText: 'Ok',
+      });
+      this.router2.navigate(['admin/carros'], {
+        state: { carroEditado: this.carro },
+      });
+    } else {
+      Swal.fire({
+        title: 'Sucesso!',
+        text: 'ADICIONADO COM SUCESSO',
+        icon: 'success',
+        confirmButtonText: 'Ok',
+      });
+      this.router2.navigate(['admin/carros'], {
+        state: { carroNovo: this.carro },
+      });
+    }
+  }
+  isEditMode = false;
+  ngOnInit(): void {
+    const id = this.router.snapshot.paramMap.get('id');
+    this.isEditMode = !!id;
   }
 }
-
--- src/app/components/carroslist/carroslist.component.ts
+++ src/app/components/carroslist/carroslist.component.ts
@@ -2,48 +2,77 @@ import { Component } from '@angular/core';
 import { Carro } from '../../models/carro';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carroslist',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './carroslist.component.html',
-  styleUrl: './carroslist.component.scss'
+  styleUrl: './carroslist.component.scss',
 })
 export class CarroslistComponent {
-
   lista: Carro[] = [];
 
-  constructor()
-  {
+  constructor() {
     let carro: Carro = new Carro();
     carro.id = 1;
     carro.nome = 'Fiesta';
- 
-    
+
     let carro2: Carro = new Carro();
     carro2.id = 2;
     carro2.nome = 'UNO';
 
-
     let carro3: Carro = new Carro();
     carro3.id = 3;
     carro3.nome = 'Monza';
- 
 
     let carro4: Carro = new Carro();
     carro4.id = 4;
     carro4.nome = 'Corolla';
 
-
     this.lista.push(carro);
     this.lista.push(carro2);
     this.lista.push(carro3);
     this.lista.push(carro4);
+
+    let carroNovo = history.state.carroNovo;
+    let carroEditado = history.state.carroEditado;
+
+    if (carroNovo) {
+      carroNovo.id = Date.now();
+      this.lista.push(carroNovo);
+    }
+
+    if (carroEditado) {
+      let indice = this.lista.findIndex((x) => {
+        return x.id == carro.id;
+      });
+      this.lista[indice] = carroEditado;
+    }
   }
 
-  deletar()
-  {
-    console.log("Teste")
+  deletar(carro: Carro) {
+    Swal.fire({
+      title: 'Tem certeza que deseja deletar esta registro?!',
+      icon: 'warning',
+      showConfirmButton: true,
+      showDenyButton : true,
+      confirmButtonText: 'Sim',
+      cancelButtonText:"Não"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let indice = this.lista.findIndex((x) => {
+          return x.id == carro.id;
+        });
+        this.lista.splice(indice, 1);
+
+            Swal.fire({
+            title: 'Deletado com Sucesso',
+            icon: 'success',
+            confirmButtonText: 'Ok',
+          })
+      }
+    });
   }
 }
